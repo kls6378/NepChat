4c59f8a72e202972b8bbbb7512966a8ffd229c56
kls6378 <kls6378@naver.com>
2019-07-21 23:49:03 +0900

    Create main and will complete animation

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('/',(req,res)=>{
-    res.send('Hello World!')
+    res.sendFile(__dirname+'/public/html/main.html')
 })
 
 app.get('/game',(req,res)=>{
     res.sendFile(__dirname+'/public/game/NeptuneChat.html')
+})
+
+app.get('/community',(req,res)=>{
+    res.sendFile(__dirname+'/public/html/community.html')
 })
 
 app.listen(8888,()=>{
