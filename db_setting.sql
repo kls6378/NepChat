10c069c1221a503763134bd3d50e329a2b2bfece
kls6378 <kls6378@naver.com>
2019-07-26 19:59:26 +0900

    Insert board information using mysql

--- db_setting.sql
+++ db_setting.sql
@@ -1,8 +1,12 @@
 CREATE DATABASE NepChat;
 
+USE NepChat;
+
 CREATE TABLE board(
     _id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(32) NOT NULL,
     password VARCHAR(32) NOT NULL,
-    title 
+    title TINYTEXT NOT NULL,
+    description TEXT NOT NULL,
+    date DATE NOT NULL
 );
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
 const dbConfig = require('./config/db_config.js')
-const db = mysql.createConnection(dbConfig)
+const connection = mysql.createConnection(dbConfig)
 
 const port = 8888
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended : false}))
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/public/html/main.html')
@@ -16,16 +18,24 @@ app.get('/game',(req,res)=>{
     res.sendFile(__dirname+'/public/game/NeptuneChat.html')
 })
 
-app.get('/community',(req,res)=>{
-    res.sendFile(__dirname+'/public/html/community.html')
+app.get('/board',(req,res)=>{
+    res.sendFile(__dirname+'/public/html/board.html')
 })
 
-app.get('/board_create',(req,res)=>{
+app.get('/board/create',(req,res)=>{
     res.sendFile(__dirname + '/public/html/board_create.html')
 })
 
-app.post('/board_create_process',(req,res)=>{
+app.get('/board/:boardIds',(req,res)=>{
+})
 
+app.post('/board/create_process',(req,res)=>{
+    connection.query(`INSERT INTO board (name, password, title, description, date) VALUES (?, MD5(?), ?, ?, NOW());`, [req.body.name, req.body.password, req.body.title, req.body.description],(err,results)=>{
+        if(err){
+            throw err
+        }
+        res.redirect(`/board/${results.insertId}`)
+    })
 })
 
 app.listen(port,()=>{
